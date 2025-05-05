132bfd2f4539c3499c06358e818b41185446ab62
Jonathan Lu <jonathan.lu31@gmail.com>
2025-05-05 11:03:16 -0700

    Update prepare_lora script, working now

--- scripts/prepare_lora.sh
+++ scripts/prepare_lora.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 
-set -x
-
 cd "$(dirname "$0")"
 
 CHECKPOINT_DIR=${1} # something like checkpoint-1500
 BASE_MODEL=${2}
 FINAL_MODEL_NAME=${3}
 
-accelerate merge-weights $CHECKPOINT_DIR --output_dir $CHECKPOINT_DIR/adapter
-mv $CHECKPOINT_DIR/adapter/model.safetensors $CHECKPOINT_DIR/adapter/adapter_model.safetensors
-cp $CHECKPOINT_DIR/../lora_init/adapter_config.json $CHECKPOINT_DIR/adapter/adapter_config.json
+CHECKPOINT_DIR="$(realpath $CHECKPOINT_DIR)"
+REPO_PATH="$(realpath $CHECKPOINT_DIR/..)"
 
-cp $CHECKPOINT_DIR/../tokenizer.json $CHECKPOINT_DIR/../merged/tokenizer.json
-cp $CHECKPOINT_DIR/../tokenizer_config.json $CHECKPOINT_DIR/../merged/tokenizer_config.json
-cp $CHECKPOINT_DIR/../special_tokens_map.json $CHECKPOINT_DIR/../merged/special_tokens_map.json
-python merge_adapter.py $BASE_MODEL $CHECKPOINT_DIR/adapter $CHECKPOINT_DIR/../$FINAL_MODEL_NAME
+accelerate merge-weights $CHECKPOINT_DIR/pytorch_model_fsdp_0 $CHECKPOINT_DIR/adapter
+mv $CHECKPOINT_DIR/adapter/model.safetensors $CHECKPOINT_DIR/adapter/adapter_model.safetensors
+cp $REPO_PATH/lora_init/adapter_config.json $CHECKPOINT_DIR/adapter/adapter_config.json
+
+mkdir -p $REPO_PATH/$FINAL_MODEL_NAME
+cp $REPO_PATH/tokenizer.json $REPO_PATH/$FINAL_MODEL_NAME/tokenizer.json
+cp $REPO_PATH/tokenizer_config.json $REPO_PATH/$FINAL_MODEL_NAME/tokenizer_config.json
+cp $REPO_PATH/special_tokens_map.json $REPO_PATH/$FINAL_MODEL_NAME/special_tokens_map.json
+python merge_adapter.py $BASE_MODEL $CHECKPOINT_DIR/adapter $REPO_PATH/$FINAL_MODEL_NAME
