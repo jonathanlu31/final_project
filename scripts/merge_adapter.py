8a13de3ef1918bad8f6e8ffffb92bc813023334a
Jonathan Lu <jonathan.lu31@gmail.com>
2025-05-04 17:41:22 -0700

    Add script to merge adapter and get the final model from our training scripts, might not work though

--- scripts/merge_adapter.py
+++ scripts/merge_adapter.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("base_model", type=str)
-parser.add_argument("lora_adapter", type=str)
+parser.add_argument("lora_adapter", type=str, help="Path to lora adapter or huggingface repo. The folder must have adapter_config.json and adapter_model.safetensors or adapter_model.bin")
 parser.add_argument("output_dir", type=str)
 args = parser.parse_args()
 
